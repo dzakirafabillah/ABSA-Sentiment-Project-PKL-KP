5bff81bbc85d0a7cf7672eacee8b021fbb75aecc
AlvinAndrian <80727692+alvinrrizky@users.noreply.github.com>
2022-08-23 11:57:31 +0700

    Update input 1 dan 2

--- frontend/main.py
+++ frontend/main.py
@@ -1,25 +1,49 @@
 import streamlit as st
-from app import predict_sentiment
-# from ../backend/app import predict_sentiment
+import requests
+# from ..backend.app import predict_sentiment_all_emiten
+
+def process_one(news : str):
+
+    r = requests.post("http://127.0.0.1:8000/predict_sentiment_all_emiten/?news="+news)
+    
+    return r
+
+def process_two(news : str, aspect : str):
+
+    t = requests.post(f"http://127.0.0.1:8000/predict_sentiment_specific_emiten/?news={news}&aspect={aspect}")
+    
+    return t
 
 # UI Layout
-st.title("Sentiment Analysis")
-st.subheader("Sentiment analysis of Indonesian stock company articles with BERT")
 
-menu = ["Home","About"]
+menu = ["1 Input","2 Input"]
 choice = st.sidebar.selectbox("Menu",menu)
 
-if choice == "Home":
-    st.subheader("Home")
+if choice == "1 Input":
+    st.title("Sentiment Analysis")
+    st.subheader("Sentiment analysis of Indonesian stock company articles with BERT")
+
+    st.subheader("1 Input for Sentiment Analysis")
     with st.form(key='nlpForm'):
-        news_text = st.text_area("Enter Article Here")
-        aspect_text = st.text_input("Enter Aspect Here")
+        news = st.text_area("Enter Article Here")
         submit_button = st.form_submit_button(label='Analyze')
         
         if submit_button:
-            predict = predict_sentiment(news_text, aspect_text)
-            st.write(predict)
-
-	# layout
-    if submit_button:
-        st.info("Results")
+            st.info("Results")
+            predict = process_one(news)
+            st.json(predict.text)
+            
+elif choice == "2 Input":
+    st.title("Sentiment Analysis")
+    st.subheader("Sentiment analysis of Indonesian stock company articles with BERT")
+    
+    st.subheader("2 Input for Sentiment Analysis")
+    with st.form(key='nlpForm'):
+        news = st.text_area("Enter Article Here")
+        aspect = st.text_input("Enter Aspect Here")
+        submit_button = st.form_submit_button(label='Analyze')
+        
+        if submit_button:
+            st.info("Results")
+            predict = process_two(news, aspect)
+            st.json(predict.text)
