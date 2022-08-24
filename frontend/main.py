481f897ea1d74cdedc5c2fcbb868e28ad6f0bfa6
Restu Triwahyuny <80731252+restutriw@users.noreply.github.com>
2022-08-24 12:36:19 +0700

    ubah tampilan fe

--- frontend/main.py
+++ frontend/main.py
@@ -1,53 +1,65 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import requests
+# from ..backend.app import predict_sentiment_all_emiten
 
-def process_one(news):
+def process_one(news : str):
+
     r = requests.post(f"http://127.0.0.1:8000/predict_sentiment_all_emiten/?news={news}")
+    
     return r
 
-def process_two(news, aspect):
-    t = requests.post(f"http://127.0.0.1:8000/predict_sentiment_specific_emiten?news={news}&aspect={aspect}")
+def process_two(news : str, aspect : str):
+
+    t = requests.post(f"http://127.0.0.1:8000/predict_sentiment_specific_emiten/?news={news}&aspect={aspect}")
+    
     return t
 
 # UI Layout
-menu = ["Input Artikel [1 Input]","Input Artikel dan Emiten [2 Input]"]
-choice = st.sidebar.selectbox("Menu",menu)
 
-if choice == "Input Artikel [1 Input]":
-    st.title("Sentiment Analysis")
-    st.subheader("Sentiment analysis of Indonesian stock company articles with BERT")
+with st.sidebar:
+    choose = option_menu("Predict", ["All Emiten", "Specific Emiten"],
+                         icons=['grid fill', 'search heart'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "20px"}, 
+        "nav-link": {"font-size": "17px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
-    st.subheader("1 Input for Sentiment Analysis")
+if choose == "All Emiten":
+    st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Sentiment Analysis</p>', unsafe_allow_html=True)    
+    st.subheader("Sentiment Analysis of Indonesian Stock Company Articles with BERT.")
+
+    st.caption("Predict Sentiment All Emiten")
     with st.form(key='nlpForm'):
-        news = st.text_area("Enter Article Here", height=200)
+        news = st.text_area("Enter Article Here")
         submit_button = st.form_submit_button(label='Analyze')
+        
         if submit_button:
             st.info("Results")
-            try:
-                predict = process_one(news)
-                st.json(predict.text)
-            except:
-                st.write("""
-                         ## SYSTEM ERROR
-                         - Check FastAPI connection
-                         - Check the input entered""")
-                
-elif choice == "Input Artikel dan Emiten [2 Input]":
-    st.title("Sentiment Analysis")
-    st.subheader("Sentiment analysis of Indonesian stock company articles with BERT")
+            predict = process_one(news)
+            st.json(predict.text)
+
+elif choose == "Specific Emiten":
+    st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Sentiment Analysis</p>', unsafe_allow_html=True)    
+    st.subheader("Sentiment Analysis of Indonesian Stock Company Articles with BERT.")
     
-    st.subheader("2 Input for Sentiment Analysis")
+    st.caption("Predict Sentiment Specific Emiten")
     with st.form(key='nlpForm'):
-        news = st.text_area("Enter Article Here", height=200)
+        news = st.text_area("Enter Article Here")
         aspect = st.text_input("Enter Aspect Here")
         submit_button = st.form_submit_button(label='Analyze')
+        
         if submit_button:
             st.info("Results")
-            try:
-                predict = process_two(news, aspect)
-                st.json(predict.text)
-            except:
-                st.write("""
-                         ## SYSTEM ERROR
-                         - Check FastAPI connection
-                         - Check the input entered""")
+            predict = process_two(news, aspect)
+            st.json(predict.text)        
